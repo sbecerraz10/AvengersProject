b45195b19223b078599c23dec91e27f3eddb4da6
sebastian <sebastian@172.30.158.128>
2018-11-14 15:49:13 -0500

    Index

---
+++ Proyecto/src/controller/IndexController.java
@@ -0,0 +1,17 @@
+package controller;
+
+public class IndexController {
+
+	
+	
+	
+	public IndexController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void initialize() {
+		
+	}
+	
+
+}
