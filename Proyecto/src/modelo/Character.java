436795267c0e9cb39ed546d468eaddf1530be07a
sebastian <sebastian@172.30.158.128>
2018-11-14 17:23:59 -0500

    BECERRA

---
+++ Proyecto/src/modelo/Character.java
@@ -0,0 +1,79 @@
+package modelo;
+
+public class Character {
+
+	private int life;
+	
+	private String nickname;
+	
+	private String image;
+	
+	private Character next;
+
+	private int posx;
+	
+	private int posy;
+	
+	public Character(int life, String nickname, String image) {
+		super();
+		this.life = life;
+		this.nickname = nickname;
+		this.image = image;
+		this.next = null;
+	}
+
+	public int getPosx() {
+		return posx;
+	}
+
+	public void setPosx(int posx) {
+		this.posx = posx;
+	}
+
+	public int getPosy() {
+		return posy;
+	}
+
+	public void setPosy(int posy) {
+		this.posy = posy;
+	}
+
+	public int getLife() {
+		return life;
+	}
+
+	public void setLife(int life) {
+		this.life = life;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public Character getNext() {
+		return next;
+	}
+
+	public void setNext(Character next) {
+		this.next = next;
+	}
+	
+	
+	
+	
+	
+	
+
+}
