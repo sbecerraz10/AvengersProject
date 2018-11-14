b522472ce5c8bb6157ac8150c4fe45a65c2e20f4
KAMILO <KAMILO@172.30.161.147>
2018-11-14 17:26:56 -0500

    Camilo -interfaz

--- Proyecto/src/controller/IndexController.java
+++ Proyecto/src/controller/IndexController.java
@@ -1,16 +1,52 @@
 package controller;
 
-public class IndexController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import modelo.Index;
+
+public class IndexController implements Initializable{
 	
+	@FXML
+    private ImageView campo1;
+
+    @FXML
+    private ImageView campo2;
+
+    @FXML
+    private ImageView campo3;
+
+    @FXML
+    private ImageView campo4;
+
+    @FXML
+    private ImageView personaje1;
+
+    @FXML
+    private ImageView personaje2;
+
+    @FXML
+    private ImageView personaje3;
+
+    @FXML
+    private ImageView personaje4;
+    
+    private Index index;
 	
-	
-	public IndexController() {
-		// TODO Auto-generated constructor stub
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
 	}
 	
-	public void initialize() {
-		
+	public void receiveIndex(Index index) {
+		this.index = index;
+	}
+	
+	public void actPage() {
+
 	}
 	
 
--- Proyecto/src/controller/MenuController.java
+++ Proyecto/src/controller/MenuController.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
+import exception.NicknameNotValid;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,14 +14,20 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.media.MediaException.Type;
 import javafx.stage.Stage;
+import modelo.Index;
 
 public class MenuController implements Initializable{
+	
+	private Index index;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		
+		index = new Index();
 	}
 	
 	@FXML
@@ -31,8 +38,10 @@ public class MenuController implements Initializable{
 		Optional<String> result = dialog.showAndWait();
 		if(result.isPresent()) {
 			try {
+			String nickName = result.get();
+			index.registrerUser(nickName);
 			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(getClass().getResource("application/Nombre.fxml"));
+			loader.setLocation(getClass().getResource("/application/IndexWindow.fxml"));
 			Parent root = loader.load();
 			Scene scene = new Scene(root);
 			Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -41,6 +50,9 @@ public class MenuController implements Initializable{
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			} catch (NicknameNotValid e) {
+				Alert a = new Alert(AlertType.ERROR);
+				a.show();
 			}
 		}
 	}
