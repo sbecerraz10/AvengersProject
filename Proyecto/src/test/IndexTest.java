bafed781f43e20e81e56544e57697ff8f659500c
KAMILO <KAMILO@172.30.161.147>
2018-11-19 10:15:48 -0500

    Camilo

--- Proyecto/src/test/IndexTest.java
+++ Proyecto/src/test/IndexTest.java
@@ -4,8 +4,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class IndexTest {
+import exception.NicknameNotValid;
+import modelo.Index;
 
+class IndexTest {
+	
+	private Index index;
+	
+	public void setUpEscenario(){
+		index = new Index();
+	}
+	
+	@Test
+	public void registrerUserTest() throws NicknameNotValid{
+		boolean tst = false;
+		setUpEscenario();
+		index.registrerUser("Bartolomeo");
+		assertTrue(tst);
+		
+	}
 	@Test
 	void test() {
 		fail("Not yet implemented");
